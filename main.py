831307e4b51e325c27253b67929fa1725fffe2a4
Leon van Wissen <leon@vwissen.nl>
2022-09-30 19:56:00 +0200

    feat: add thesaurus and extra functions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ l.vanwissen@uva.nl
 import time
 import requests
 from json.decoder import JSONDecodeError
+from isodate import parse_date
 
 import rdflib
 from rdflib import RDF, Namespace, BNode, URIRef, Literal
@@ -13,42 +14,112 @@ from rdflib import RDF, Namespace, BNode, URIRef, Literal
 SCHEMA = Namespace("https://schema.org/")
 
 
-def main(filepath: str, destination: str) -> None:
+def main(filepath: str, destination: str, thesaurusfile: str = "") -> None:
 
-    # Load the file from the RKD
-    print("1/5 Loading the file into a memory store graph")
-    g = loadRDF(filepath)
+    # Load the file from the RKD, and load the thesaurus (in SKOS) separately
+    print("1/7 Loading the file(s) into a memory store graph")
+    g = loadRDF(filepath, thesaurusfile)
+
+    # Remove all the wrong labels from the thesaurus entries
+    print("2/7 Removing all the thesaurus labels")
+    g = removeLabels(g)
 
     # Add a URL of the RKD website to the resources
-    print("2/5 Adding the URL to the public permalink")
+    print("3/7 Adding the URL to the public permalink")
     g = addURL(g)
 
     # Correct the roles to schema.Role objects
-    print("3/5 Correcting the schema:additionalType statements")
+    print("4/7 Correcting the schema:additionalType statements")
     g = correctRoles(g)
 
     # Add depictions to experpts from RKD API
-    print("4/5 Adding a URI of a thumbnail")
+    print("5/7 Adding a URI of a thumbnail")
     g = addImages(g)
 
-    # Save the enriched file
-    print("5/5 Saving the graph to a file")
+    # Find incorrect dates
+    print("6/7 Get a list of incorrectly formatted dates and remove these")
+    g = fixDates(g)
+
+    # Save the enriched/corrected file
+    print("7/7 Saving the graph to a file")
     g.serialize(destination, format="turtle")
 
 
-def loadRDF(filepath: str) -> rdflib.ConjunctiveGraph:
+def loadRDF(filepath: str, thesauruspath: str = "") -> rdflib.ConjunctiveGraph:
     """
     Load in the RDF file (in RDF/XML) and return a graph object.
 
     Args:
         filepath: The path to the RDF file.
+        thesauruspath: The path to the RDF thesaurus file.
 
     Returns:
         A graph object.
     """
 
     g = rdflib.ConjunctiveGraph()
+
     g.parse(filepath)
+
+    if thesauruspath:
+        g.parse(thesauruspath)
+
+    return g
+
+
+def removeLabels(g: rdflib.ConjunctiveGraph) -> rdflib.ConjunctiveGraph:
+    """
+    Due to a quirk in Adlib and the export and conversion script
+    the thesaurus references have gotten multiple schema:name entries.
+    A URI such as <https://data.rkd.nl/thesaurus/6> not only has the
+    name "painter", but also has 100+ other names that are incorrect.
+    In this function we remove these labels, since we have converted the RKD thesaurus separately. The name of the role of the
+
+    Args:
+        g: The graph object.
+
+    Returns:
+        The graph object without thesaurus labels.
+    """
+
+    q = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX schema: <https://schema.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+    SELECT DISTINCT * WHERE { 
+        ?thesaurus schema:name ?label .
+        
+        FILTER(CONTAINS(STR(?thesaurus), 'thesaurus'))
+
+        OPTIONAL {
+            ?thesaurus skos:prefLabel ?labelEN .
+                FILTER(LANG(?labelEN) = 'en')
+        }
+
+        OPTIONAL {
+            ?thesaurus skos:prefLabel ?labelNL .
+                FILTER(LANG(?labelNL) = 'nl')
+        }
+            
+    }
+    """
+
+    results = g.query(q)
+
+    for r in results:
+
+        # Remove all the wrong ones
+        g.remove((r.thesaurus, SCHEMA.name, r.label))
+
+        # Add back the right ones to stick to the schema vocabulary
+        if r.labelEN:
+            g.add((r.thesaurus, SCHEMA.name, Literal(r.labelEN, lang="en")))
+
+        if r.labelNL:
+            g.add((r.thesaurus, SCHEMA.name, Literal(r.labelNL, lang="nl")))
+
     return g
 
 
@@ -99,6 +170,7 @@ def correctRoles(g: rdflib.ConjunctiveGraph) -> rdflib.ConjunctiveGraph:
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX schema: <https://schema.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
     SELECT * WHERE {
         ?manuscript a schema:Manuscript ;
@@ -111,12 +183,12 @@ def correctRoles(g: rdflib.ConjunctiveGraph) -> rdflib.ConjunctiveGraph:
             ?person schema:additionalType ?additionalType .
 
             OPTIONAL {
-                ?additionalType schema:name ?labelEN .
+                ?additionalType skos:prefLabel ?labelEN .
                 FILTER(LANG(?labelEN) = 'en')
             }
 
             OPTIONAL {
-                ?additionalType schema:name ?labelNL .
+                ?additionalType skos:prefLabel ?labelNL .
                 FILTER(LANG(?labelNL) = 'nl')
             }
         }
@@ -126,6 +198,8 @@ def correctRoles(g: rdflib.ConjunctiveGraph) -> rdflib.ConjunctiveGraph:
     results = g.query(q)
 
     for r in results:
+
+        print(r.additionalType)
 
         g.remove((r.manuscript, SCHEMA.about, r.person))
 
@@ -234,10 +308,52 @@ def getAPI(identifier: str, retry=False) -> dict:
         return getAPI(identifier, retry=True)
 
 
+def fixDates(g: rdflib.ConjunctiveGraph) -> rdflib.ConjunctiveGraph:
+    """
+    Find and remove any incorrectly formatted dates that make a
+    triplestore crash. This function prints the excerpt URI and
+    the date to the console.
+
+    Args:
+        g: The graph object.
+
+    Returns:
+        The graph object without the incorrect dates
+    """
+
+    q = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX schema: <http://schema.org/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    SELECT * WHERE { 
+
+        ?excerpt schema:contentReferenceTime ?sub .
+
+        ?sub ?pred ?literal .
+    
+        FILTER(DATATYPE(?literal) = xsd:date)
+
+    }
+    """
+
+    results = g.query(q)
+
+    for r in results:
+        try:
+            parse_date(r.literal)
+        except ValueError:
+            print(r.excerpt, r.literal)
+            g.remove((r.subject, r.predicate, r.literal))
+
+    return g
+
+
 if __name__ == "__main__":
 
     FILEPATH = "data/20220926_BrediusExportVolledig.xml"
+    THESAURUSPATH = "data/rkdthesaurus.trig"
     # FILEPATH = "data/20210721brediusexportgoldenagents.xml"
-    DESTINATION = "data/ga_20210721brediusexportgoldenagents.ttl"
+    DESTINATION = "data/ga_20220926_BrediusExportVolledig.ttl"
 
     main(FILEPATH, DESTINATION)
